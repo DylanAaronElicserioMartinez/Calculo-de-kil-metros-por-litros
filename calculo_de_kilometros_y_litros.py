e9ac7ff5c3d9efb67af8651b2d58a0ee5db4183a
DylanAaronElicserioMartinez <a.23308051281179@cbtis128.edu.mx>
2024-09-23 20:27:24 -0600

    Add files via upload

---
+++ calculo_de_kilometros_y_litros.py
@@ -0,0 +1,16 @@
+print(" ")
+print("Dylan Aaron Elicserio Martínez esta es mi practica:")
+print(" ")
+#Solicitar al usuario la cantidad de kilometros recorridos con su moto
+numero1=input("ingresa la cantidad de kilometros recorridos con su moto:")
+#Conversion del primer numero caracter entero
+num1=int(numero1)
+#Solicitar al usuario la cantidad de litros utilizados durante su viaje
+numero2=input("ingresa la cantidad de litros consumidos:")
+#Conversion del segundo numero a caracter entero
+num2=int(numero2)
+#divide la cantidad de kilometros  recorridos con los litros de combustible utilizado
+division=num1/num2
+#Muestra el resultado del calculo de cuanto es el consumo de gasolina por kilometro
+resultado=print("El resultado de cuanto gasta por kilometro su moto en litros es:")
+print(division)
